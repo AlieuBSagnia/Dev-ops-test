eb191b434c9a051c2b178c82afb14de5248edb38
AlieuBSagnia <alieubsagnia@gmail.com>
2020-02-23 19:13:34 +0000

    Done

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "name" {
+  default="app-aaron"
+}
+
+variable "app_ami_id" {
+  default="ami-031addfc78bcdf57d"
+}
+
+variable "db_ami_id" {
+  default="ami-0f8fd53560ca320f6"
+}
+
+variable "cidr_block" {
+  default="10.17.0.0/16"
+}
